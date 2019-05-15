11da2f0d9ab35f0c5b1f7b0d683aee1281bf197f
Maksim Khomutov <mkhomutov@gmail.com>
2019-05-15 16:31:24 +0400

    change target platform to net46

--- src/NuGetPackageManager/MainWindow.xaml.cs
+++ src/NuGetPackageManager/MainWindow.xaml.cs
@@ -1,33 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace NuGetPackageManager
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        #region Constructors
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void ButtonExit_Click(object sender, RoutedEventArgs e)
-        {
-            Application.Current.Shutdown();
-        }
+        #endregion
     }
 }
