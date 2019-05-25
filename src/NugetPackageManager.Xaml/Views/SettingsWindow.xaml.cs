687a7ee92c219af1e911cfa0c24fb68c33e08f1d
Artem <art.saikin@gmail.com>
2019-05-26 00:33:48 +0400

    nuget settings; added usercontrol for nuget settings

---
+++ src/NugetPackageManager.Xaml/Views/SettingsWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace NugetPackageManager.Xaml.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для SettingsWindow.xaml
+    /// </summary>
+    public partial class SettingsWindow : Catel.Windows.DataWindow
+    {
+        public SettingsWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
