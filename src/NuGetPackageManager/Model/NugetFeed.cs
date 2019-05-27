0e9a7b3f84c1da77e2ea2b9d071f9c34e7f9d053
Artem <art.saikin@gmail.com>
2019-05-27 23:39:18 +0400

    Basic actions like add/remove feeds; all command bindings

---
+++ src/NuGetPackageManager/Model/NugetFeed.cs
@@ -0,0 +1,24 @@
+﻿using Catel.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NuGetPackageManager.Model
+{
+    public class NugetFeed : ModelBase
+    {
+        public NugetFeed(string name, string source)
+        {
+            Name = name;
+            Source = source;
+        }
+
+        public string Name { get; set; }
+
+        public string Source { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}
--- src/NugetPackageManager.Xaml/ViewModels/SettingsControlViewModel.cs
+++ src/NugetPackageManager.Xaml/ViewModels/SettingsControlViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using Catel.MVVM;
+using NuGetPackageManager.Model;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,5 +11,87 @@ namespace NugetPackageManager.Xaml.ViewModels
 {
     public class SettingsControlViewModel : ViewModelBase
     {
+        public SettingsControlViewModel()
+        {
+            Title = "Settings";
+        }
+
+        #region commands
+
+        public Command UpdateFeed { get; set; }
+
+        public Command RemoveFeed { get; set; }
+
+        public Command MoveUpFeed { get; set; }
+
+        public Command MoveDownFeed { get; set; }
+
+        public Command AddFeed { get; set; }
+
+        public Command OpenChooseLocalPathToSourceDialog { get; set; }
+
+        #endregion
+
+        public ObservableCollection<NugetFeed> Feeds = new ObservableCollection<NugetFeed>();
+
+        public string Name { get; set; }
+
+        public string Source { get; set; }
+
+        public NugetFeed SelectedFeed { get; set; }
+
+        protected override Task InitializeAsync()
+        {
+            CommandInitialize();
+            return base.InitializeAsync();
+        }
+
+        protected void CommandInitialize()
+        {
+            UpdateFeed = new Command(OnSaveOrUpdateFeed);
+            RemoveFeed = new Command(OnRemoveFeed);
+            MoveUpFeed = new Command(OnMoveUpFeed);
+            MoveDownFeed = new Command(OnMoveDownFeed);
+            AddFeed = new Command(OnAddFeed);
+            OpenChooseLocalPathToSourceDialog = new Command(OnOpenChooseLocalPathToSourceDialog);
+        }
+
+
+        #region command actions
+
+        private void OnSaveOrUpdateFeed()
+        {
+
+        }
+
+        private void OnRemoveFeed()
+        {
+            Feeds.Remove(SelectedFeed);
+        }
+
+        private void OnMoveUpFeed()
+        {
+     
+        }
+
+        private void OnMoveDownFeed()
+        {
+
+        }
+
+        private void OnAddFeed()
+        {
+            Feeds.Add(new NugetFeed(namePlaceholder, sourcePlaceholder));
+        }
+
+        private void OnOpenChooseLocalPathToSourceDialog()
+        {
+
+        }
+
+        #endregion 
+
+        const string namePlaceholder = "Package source";
+        const string sourcePlaceholder = "http://packagesource";
     }
 }
