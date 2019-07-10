b47f2e907cbc16bb0d4055cccca42901e634e0fa
Artem Saikin <art.saikin@gmail.com>
2019-07-10 09:28:15 +0400

    some ui improvements inside packages page

--- src/NuGetPackageManager/Web/IconDownloader.cs
+++ src/NuGetPackageManager/Web/IconDownloader.cs
@@ -15,7 +15,7 @@ namespace NuGetPackageManager.Web
         //readonly HttpSource httpSource = new HttpSource();
         public IconDownloader()
         {
-            //this can be danger and considered harmful
+            //this can be danger and considered harmful (we trust to all cerfs)
             //more info https://stackoverflow.com/questions/703272/could-not-establish-trust-relationship-for-ssl-tls-secure-channel-soap/6613434#6613434
             ServicePointManager.ServerCertificateValidationCallback = (s, cert, chain, ssl) => true;
             ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
