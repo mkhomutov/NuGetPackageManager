c49855becf343c63b0bc2206d7607db1509fb638
Artem Saikin <art.saikin@gmail.com>
2019-08-08 13:42:37 +0400

    move inrefaces; common manager interface for extensibility

---
+++ src/NuGetPackageManager/Interface/IManagerExtensible.cs
@@ -0,0 +1,22 @@
+﻿using NuGetPackageManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NuGetPackageManager
+{
+    public interface IManagerExtensible
+    {
+        string Name { get; }
+
+        IReadOnlyList<NuGetPackage> PackageList { get; }
+
+        void Install();
+
+        void Update();
+
+        void Uninstall();
+    }
+}
---
+++ src/NuGetPackageManager/Management/ExampleFolderPackageManagement.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NuGetPackageManager.Models;
+
+namespace NuGetPackageManager.Management
+{
+    public class ExampleFolderPackageManagement : IManagerExtensible
+    {
+        public string Name => "Test_Folder";
+
+        public IReadOnlyList<NuGetPackage> PackageList { get; set; }
+
+        public void Install()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Uninstall()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
