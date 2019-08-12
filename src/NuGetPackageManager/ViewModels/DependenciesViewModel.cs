c017cc1ffdbf8d91815f9a6091e9474f97bc5dd4
Artem Saikin <art.saikin@gmail.com>
2019-08-12 11:12:41 +0400

    dependencies view & viewmodel

---
+++ src/NuGetPackageManager/ViewModels/DependenciesViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Catel.MVVM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NuGetPackageManager.ViewModels
+{
+    public class DependenciesViewModel : ViewModelBase
+    {
+        public DependenciesViewModel()
+        {
+
+        }
+    }
+}
