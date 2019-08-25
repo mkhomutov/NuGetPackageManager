3c33583c7e1301deb7121778ee73d32af6b9fcb4
Artem Saikin <art.saikin@gmail.com>
2019-08-25 18:19:49 +0400

    remove commented

--- src/NuGetPackageManager/ModuleInitializer.cs
+++ src/NuGetPackageManager/ModuleInitializer.cs
@@ -41,7 +41,6 @@ public static class ModuleInitializer
 
 
         serviceLocator.RegisterType<ISourceRepositoryProvider, SourceRepositoryProvider>();
-        //serviceLocator.RegisterInstance<ISourceRepositoryProvider>();
 
         serviceLocator.RegisterType<IRepositoryService, RepositoryService>();
     }
